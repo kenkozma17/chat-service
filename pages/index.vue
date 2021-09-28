a30b6b201811e229a85365dddafdb56ae44ff45d
Ken Kozma <kenkozma17@gmail.com>
2021-09-28 20:59:39 +0800

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-black">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      name: 'Ken'
+    }
+  }
+})
+</script>
